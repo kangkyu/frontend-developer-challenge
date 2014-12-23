22324fbfdbbcc4cbe6228f4d6c17388a6ec41f36
kangkyu <kangkyu1111@gmail.com>
2014-12-22 18:01:56 -0800

    rearrange files with absolute paths in the directory

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', []);
 
 app.controller('MembersCtrl', function($scope, $http) {
-  $http.get('members.json').success(function(data){
+  $http.get('/app/members.json').success(function(data){
     $scope.members = data;
   });
 });
