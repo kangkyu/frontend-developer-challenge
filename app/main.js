9cdea0cb17a7d25fd9302d79e28cd176675951d7
kangkyu <kangkyu1111@gmail.com>
2014-12-26 18:14:25 -0800

    refactor with name changes before adding route feautre

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
-var app = angular.module('app', []);
+var app = angular.module('memberApp', []);
 
-app.controller('MembersCtrl', function($scope, $http) {
-  $http.get('/app/members.json').success(function(data){
+app
+.controller('MembersIndexCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('app/members.json').success(function(data){
     $scope.members = data;
   });
-});
+}]);
+
