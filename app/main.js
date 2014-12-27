3482f19918ef17aae6b6c6e79c17399a10f1d603
kangkyu <kangkyu1111@gmail.com>
2014-12-27 12:51:48 -0800

    add business show page

--- app/main.js
+++ app/main.js
@@ -8,6 +8,8 @@ app.config(['$routeProvider', function($routeProvider) {
         controller: "MemberShowCtrl"
       }).when('/businesses', {
         templateUrl: 'app/partials/business_index.html',
+      }).when('/businesses/:businessName', {
+        templateUrl: 'app/partials/business_show.html',
       }).when('/reviews', {
         templateUrl: 'app/partials/review_index.html',
       }).otherwise({
@@ -58,3 +60,29 @@ squealControllers.controller('ReviewIndexCtrl', ['$scope', '$http', function($sc
 squealControllers.controller('BusinessIndexCtrl', function($scope) {
   $scope.message = 'This is BusinessIndexCtrl (businesses - companies) screen';
 });
+
+squealControllers.controller('BusinessShowCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    var reviews = [];
+    $scope.search = function() {
+      return $http.get('app/reviews.json').success(function(data) {
+        $scope.reviews = data;
+      });
+    }
+
+    function getById(arr, name) {
+      for (var d=0; d < arr.length; d +=1) {
+        if (arr[d].businessName === name) {
+          reviews.push(arr[d])
+          return reviews;
+        }
+      }
+    }
+
+    $scope.search().then(function() {
+      $scope.reviews = getById($scope.reviews, $routeParams.businessName);
+    });
+
+    $scope.businessName = $routeParams.businessName;
+
+}]);
