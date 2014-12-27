95e3d60b44e014b38b9a689f82126bd463d2506d
kangkyu <kangkyu1111@gmail.com>
2014-12-26 21:29:05 -0800

    add routing on main.js file

--- app/main.js
+++ app/main.js
@@ -1,9 +1,36 @@
-var app = angular.module('memberApp', []);
+var app = angular.module('memberApp', ['ngRoute']);
 
-app
-.controller('MembersIndexCtrl', ['$scope', '$http', function($scope, $http) {
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/members', {
+        templateUrl: 'app/partials/member_index.html',
+        controller: 'MemberIndexCtrl'
+      }).when('/businesses', {
+        templateUrl: 'app/partials/business_index.html',
+        controller: 'BusinessIndexCtrl'
+      }).when('/reviews', {
+        templateUrl: 'app/partials/review_index.html',
+        controller: 'ReviewIndexCtrl'
+      }).otherwise({
+        redirectTo: '/members'
+      });
+  }]);
+
+app.controller('MemberIndexCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.query = {};
+
   $http.get('app/members.json').success(function(data){
     $scope.members = data;
   });
 }]);
 
+app.controller('ReviewIndexCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('app/reviews.json').success(function(data){
+    $scope.reviews = data;
+  });
+}]);
+
+
+app.controller('BusinessIndexCtrl', function($scope) {
+
+  $scope.message = 'This is BusinessIndexCtrl (businesses - companies) screen';
+});
